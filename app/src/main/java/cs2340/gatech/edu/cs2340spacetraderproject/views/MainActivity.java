2e2d262fd15907fac8a6a1f2d8b19e57aff80d9f
Saira <sairapoonnen@gmail.com>
2019-02-14 16:45:15 -0500

    "Update model packages"

--- app/src/main/java/cs2340/gatech/edu/cs2340spacetraderproject/views/MainActivity.java
+++ app/src/main/java/cs2340/gatech/edu/cs2340spacetraderproject/views/MainActivity.java
@@ -1,7 +1,9 @@
-package cs2340.gatech.edu.cs2340spacetraderproject;
+package cs2340.gatech.edu.cs2340spacetraderproject.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cs2340.gatech.edu.cs2340spacetraderproject.R;
 
 public class MainActivity extends AppCompatActivity {
 

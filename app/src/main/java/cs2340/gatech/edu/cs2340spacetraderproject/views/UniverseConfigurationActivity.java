843fc93eb1716ab158104216efd485b0095c7c69
charlieye <cye45@gatech.edu>
2019-04-04 14:00:06 -0400

    Merge branch 'master' of /Users/charlieye/CS2340/CS2340SpaceTraderProject with conflicts.

--- app/src/main/java/cs2340/gatech/edu/cs2340spacetraderproject/views/UniverseConfigurationActivity.java
+++ app/src/main/java/cs2340/gatech/edu/cs2340spacetraderproject/views/UniverseConfigurationActivity.java
@@ -1,23 +1,16 @@
 package cs2340.gatech.edu.cs2340spacetraderproject.views;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import java.util.Random;
-
 import cs2340.gatech.edu.cs2340spacetraderproject.R;
 import cs2340.gatech.edu.cs2340spacetraderproject.model.SolarSystem;
 import cs2340.gatech.edu.cs2340spacetraderproject.model.Universe;
-import cs2340.gatech.edu.cs2340spacetraderproject.model.tradegoods.*;
 import cs2340.gatech.edu.cs2340spacetraderproject.viewmodels.UniverseViewModel;
+
+import java.util.Random;
 
 public class UniverseConfigurationActivity extends AppCompatActivity {
 
